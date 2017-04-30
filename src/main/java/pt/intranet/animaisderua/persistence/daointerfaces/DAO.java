661f768b36b5b2cc4295fc1a04f7b497d0b9a7e1
Sérgio Morais <sm.developer82@gmail.com>
2017-04-30 04:50:08 +0100

    structure - mySQL config

---
+++ src/main/java/pt/intranet/animaisderua/persistence/daointerfaces/DAO.java
@@ -0,0 +1,16 @@
+package pt.intranet.animaisderua.persistence.daointerfaces;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by Sérgio Morais on 30/04/2017.
+ */
+public interface DAO<T> extends Serializable {
+    public abstract T save(T entity);
+
+    public abstract T update(T entity);
+
+    public abstract List<T> listAll(T entity);
+
+}
